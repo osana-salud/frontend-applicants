49d374627982ca9f4af869ad49a4d7213532808a
Juan <jpgallegos20@gmail.com>
2020-12-09 11:17:24 -0300

    config: next.config with their domains are required to know where the images coming from. In our case, Githab and Gitlab

---
+++ next.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  images: {
+    domains: [
+      "secure.gravatar.com",
+      "assets.gitlab-static.net",
+      "avatars0.githubusercontent.com",
+      "avatars1.githubusercontent.com",
+      "avatars2.githubusercontent.com",
+      "avatars3.githubusercontent.com",
+      "avatars4.githubusercontent.com",
+      "avatars5.githubusercontent.com",
+      "avatars6.githubusercontent.com",
+      "avatars7.githubusercontent.com",
+      "avatars8.githubusercontent.com",
+    ],
+  },
+};
